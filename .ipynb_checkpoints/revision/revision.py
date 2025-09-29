a15523e7d7e7248872b1e89daac07fb49faa0237
surajkr502 <sawsurajkumar502@gmail.com>
2025-09-29 22:05:15 +0530

    revision

---
+++ .ipynb_checkpoints/revision/revision.py
@@ -0,0 +1,24 @@
+# import keyword
+# x=keyword.kwlist
+# print(len(x))
+# import string
+# punctuators=string.punctuation
+# print("punctuators=",punctuators)
+
+
+# x,y,z="suraj","niraj","saroj"
+# print(x)
+# print(y)
+# print(z)
+
+
+# city={"bhopal","ranchi","jabalpur"}
+# x=y=z=city
+# print(x)
+# print(y)
+# print(z)
+
+
+
+my_range = range(1,11,-1) 
+print(list(my_range))
