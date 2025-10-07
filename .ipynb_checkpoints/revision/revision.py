9bc6bec4f77ee3fd6c05046e23e09cf15f218a9c
surajkr502 <sawsurajkumar502@gmail.com>
2025-10-07 21:31:28 +0530

    revision

--- .ipynb_checkpoints/revision/revision.py
+++ .ipynb_checkpoints/revision/revision.py
@@ -68,5 +68,50 @@
 #         print(x,end=" ")
 #     x+=1
 
+# while loop 
+
+# print n natural no.
+# num=int(input("enter natural no.:"))
+# i=0
+# while i<=num:
+#     if i!=num:
+#         print(i,end=",")
+#     else:
+#         print(i,end="")
+#     i+=1
 
 
+# for loop
+
+# n=int(input("enter a number:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+
+# n=int(input("enter a no.:"))
+# for i in range(1,n+1):
+#     char='A'
+#     for j in range(1,i+1):
+#         print(char,end='')
+#         char=chr(ord(char)+1)
+#     print()
+
+
+# n=int(input("enter a no.:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(2*j,end='')
+#     print()
+
+# n=int(input("enter a no.:"))
+# for i in range(1,n+1):
+#     print('*'*i)
+
+# n=int(input("enter no. of rows:"))
+# for i in range(1,n+1):
+#     print(' '*(n-i),'* '*i)
+
+n=int(input("enter row count:"))
+for i in range(1,n+1):
+    print(" "*(n-i),'*'*(2*i-1))
